ae983a5f7c6644729d49284e99907c80aacf7893
Mark Simon <mesimoniv@gmail.com>
2019-10-03 11:46:05 -0500

    hard code outer radius temporarily, change css class colors for unity

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -4,11 +4,10 @@ from .models import Wheel, Segment
 class WheelForm(forms.ModelForm):
     numSegments = forms.IntegerField(label='Number of Prizes', min_value=1, max_value=99)
     innerRadius = forms.IntegerField(label='Inside Radius', min_value=0, max_value=100)
-    outerRadius = forms.IntegerField(label='Outside Radius', min_value=10, max_value=249)
 
     class Meta:
       model = Wheel
-      fields = ('name', 'description','numSegments','innerRadius','outerRadius',)
+      fields = ('name', 'description','numSegments','innerRadius',)
 
 class SegmentForm(forms.ModelForm):
     class Meta:
